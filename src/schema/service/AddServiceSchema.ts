bdb4e3447e5fd315be7972a99923cef15f5be633
moaha7 <mohammedalhajji717@gmail.com>
2025-04-28 00:58:20 -0700

    create add service hook and schema

--- src/schema/service/AddServiceSchema.ts
+++ src/schema/service/AddServiceSchema.ts
@@ -7,14 +7,11 @@ const AddServiceSchema = Yup.object().shape({
     .typeError("Price must be a number")
     .required("Price is required")
     .positive("Price must be positive"),
-  clinicAssociationId: Yup.string().required("Clinic Association is required"),
-  status: Yup.mixed<"active" | "inactive">()
-    .oneOf(["active", "inactive"], "Status must be active or inactive")
-    .required("Status is required"),
-  clinics: Yup.array()
-    .of(Yup.string().required("Clinic ID must be a string"))
-    .min(1, "At least one clinic must be selected")
-    .required("Clinics are required"),
+  clinic: Yup.string().required("Clinis is required"),
+  // array()
+  //   .of(Yup.string().required("Clinic ID must be a string"))
+  //   .min(1, "At least one clinic must be selected")
+  //   .required("Clinics are required"),
   doctors: Yup.array()
     .of(Yup.string().required("Doctor ID must be a string"))
     .min(1, "At least one doctor must be selected")
