3d36865d367b3522dcbbe07a307e63cdb65f581a
AbdelwahapBakdones <developer01.arabinterface@gmail.com>
2025-05-18 10:57:14 +0300

    search service

--- src/hooks/serviceH/useServicesList.ts
+++ src/hooks/serviceH/useServicesList.ts
@@ -5,20 +5,11 @@ import usePaginationtStore from "../../store/Pagination/usePaginationtStore";
 import ServiceDetailsType from "../../types/serviceT/ServiceDetailsType";
 const useServicesList = (allData = false, sortBy = "_id", order = "desc") => {
   const pagination = usePaginationtStore();
-  // console.log("useGetUsers per_page", per_page);
-  //   const countryStore = useCountriesPaginationStore();
+  console.log("useGetUsers per_page", pagination.paramKey);
+  // const countryStore = useCountriesPaginationStore();
 
   return useQuery({
-    queryKey: [
-      "services",
-      pagination.current_page,
-      pagination.items_per_page,
-      allData,
-      sortBy,
-      order,
-      pagination.paramKey,
-      pagination.filter,
-    ],
+    queryKey: ["services"],
     queryFn: () => {
       const url = `/services?${
         "&page=" +
@@ -28,15 +19,13 @@ const useServicesList = (allData = false, sortBy = "_id", order = "desc") => {
         "&sortBy=" +
         sortBy +
         "&order=" +
-        order 
-        // +
-        // "&search=" +
-        // pagination.paramKey 
+        order +
+        "&search=" +
+        pagination.paramKey
         // "&isActive=" +
-        // pagination.filter 
+        // pagination.filter
       }`;
-      
-      
+
       return axiosInstance
         .get<ResponseType<ServiceDetailsType>>(url)
         .then((res) => {
